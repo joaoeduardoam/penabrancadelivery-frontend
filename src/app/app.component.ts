edb2b6df9987519d76ca2e759945d522c5e8f95d
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2024-09-09 23:09:36 -0300

    feat: Adding header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HeaderComponent } from "./comps/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule ],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
