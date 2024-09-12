ef0a30917abb30c4cd8d43e2398ba16719d33d2c
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2024-09-11 23:08:50 -0300

    refact: design home/product-card

--- src/app/comps/product-card/product-card.component.ts
+++ src/app/comps/product-card/product-card.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
