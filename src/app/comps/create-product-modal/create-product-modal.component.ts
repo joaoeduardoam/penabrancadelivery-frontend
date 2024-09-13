739b4ea3f9f135af189ce65349198147166867e2
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2024-09-12 21:44:57 -0300

    feat: Adding Create Product Modal

--- src/app/comps/create-product-modal/create-product-modal.component.ts
+++ src/app/comps/create-product-modal/create-product-modal.component.ts
@@ -1,22 +1,41 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormGroupDirective, NgForm} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+// export class MyErrorStateMatcher implements ErrorStateMatcher {
+//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+//   }
+// }
 
 @Component({
   selector: 'app-create-product-modal',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './create-product-modal.component.html',
   styleUrl: './create-product-modal.component.css'
 })
 export class CreateProductModalComponent {
 
+  urlPattern = '(https?://)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)';
+
   formProduct = new FormGroup({
     title: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    description: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    price: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(10)]),
-    image: new FormControl(null, [Validators.required, Validators.minLength(10)])
+    description: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    price: new FormControl(null, [Validators.required, Validators.min(0), Validators.pattern(/^\d+(\.\d{1,2})?$/)]),
+    image: new FormControl(null, [Validators.required, Validators.pattern(this.urlPattern)])
   })
+
+  onSubmit(){
+    console.log("values", this.formProduct)
+  }
+
+  // matcher = new MyErrorStateMatcher();
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ba005',
+      },
+    },
   },
   plugins: [],
 }
