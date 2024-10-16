b806760789d7807b3f8baa5309ba43e9958aaaa4
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2024-10-16 16:52:24 -0300

    feat: Separating product card actions between ADMIN and CUSTOMER

--- src/app/comps/product-card/product-card.component.ts
+++ src/app/comps/product-card/product-card.component.ts
@@ -6,11 +6,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { UpdateProductModalComponent } from '../update-product-modal/update-product-modal.component';
 import { Product } from '../../model/Product';
 import { ProductService } from '../../services/product/product.service';
+import { AuthService } from '../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
@@ -18,8 +20,21 @@ export class ProductCardComponent {
 
   @Input() product:Product
 
-  constructor (public dialog: MatDialog, private productService:ProductService){}
+  userRole: string;
 
+  constructor (public dialog: MatDialog, private productService:ProductService, private authService:AuthService){}
+
+  ngOnInit(): void {    
+    if (typeof window !== 'undefined') {
+      this.authService.getUserProfile().subscribe(
+        (auth)=>{
+          this.userRole = auth.role
+          console.log("userRole: ", this.userRole)
+        }
+      );
+    }
+    
+  }
   openUpdateProductModal(){
     this.dialog.open(UpdateProductModalComponent, {
       data: this.product // Passando o produto para o modal
