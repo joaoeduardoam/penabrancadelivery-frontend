58be8e4e99969bf6b0f6aa417ee83bc472e096cd
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2025-02-03 22:45:25 -0300

    feat: Adding badge on the cart button

--- src/app/comps/header/header.component.ts
+++ src/app/comps/header/header.component.ts
@@ -8,29 +8,69 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { AuthService } from '../../services/auth/auth.service';
 import {MatMenuModule} from '@angular/material/menu';
 import { Router } from '@angular/router';
+import { CartService } from '../../services/cart/cart.service';
+
+import {MatBadgeModule} from '@angular/material/badge';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSlideToggleModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [MatSlideToggleModule, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatBadgeModule], 
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  constructor (public authService:AuthService, private router:Router, private cartService:CartService){}
+
   userName:any=null;
 
-  constructor (public authService:AuthService, private router:Router){}
+  numberItemsInCart = 0;
+
+  private subscription: Subscription;
+
+
   ngOnInit(): void {    
     if (typeof window !== 'undefined') {
+
       this.authService.getUserProfile().subscribe(
         (auth)=>{
           this.userName = auth.name
         }
       );
+
+      this.listenToCart();
+
     }
     
   }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+        this.subscription.unsubscribe();
+    }
+  }
+
+  listenToCart() {
+    this.subscription = this.cartService.addedToCart.subscribe({
+        next: (productsInCart) => {
+            // Check if it is an array
+            if (!Array.isArray(productsInCart)) {
+                console.error('Invalid data received');
+                return;
+            }
+
+            // Uses the optional chaining operator (?) and nullish coalescing (||)
+            this.numberItemsInCart = productsInCart.reduce((acc, product) => {
+                return acc + (product?.quantity || 0);
+            }, 0);
+        },
+        error: (error) => {
+          console.error('Error updating cart:', error);
+      }
+    });
+}
 
 
   handleLogOut(){
